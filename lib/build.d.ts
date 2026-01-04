3a8a1a6f8726241db6efd32f39aa4422b943d5bb
dDeepLb <ddeeplb@gmail.com>
2026-01-04 04:21:15 +0200

    update package.json to reference new deeplib.js, enhance BuildConfig interface, and add new CLI functionality

--- lib/build.d.ts
+++ lib/build.d.ts
@@ -1,10 +1,8 @@
 export declare interface BuildConfig {
   /** Entry file located in `src/` */
-  entry?: string,
+  entry: string,
   /** Output file located in `{distDirName}/` */
-  outfile?: string,
-  /** Global name of the mod */
-  globalName: string,
+  outfile: string,
   /** Directory name of the dist folder, where the mod will be built */
   distDirName?: string,
   /** Directory name of the public folder, where the mod assets will be copied */
@@ -12,21 +10,15 @@ export declare interface BuildConfig {
   /** `node` scripts to run */
   scripts?: string[],
   /** URL to the mod on the production server */
-  prodRemoteURL: string,
+  prodRemoteURL?: string,
   /** URL to the mod on the development server */
-  devRemoteURL: string,
-  /** Target version of the build */
-  target?: string[],
-  /** Additional esbuild plugins */
-  plugins?: import('esbuild').Plugin[],
-  /** Additional esbuild defines (like global variables) */
-  defines?: {
-    [key: string]: unknown
-  },
+  devRemoteURL?: string,
   /** Host of the local dev server */
   host?: string,
   /** Port of the local dev server */
   port?: number
+  /** Additional esbuild options (target, plugins, define, globalName, etc.) */
+  esbuildOptions?: import('esbuild').BuildOptions
 } 
 
-export declare function defineConfig(config: BuildConfig): BuildConfig;
+export declare function defineConfig(config: BuildConfig, esbuildOptions?: import('esbuild').BuildOptions): BuildConfig;
