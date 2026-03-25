401065686874d0f0e673181d16171cba8c55e6fe
dDeepLb <ddeeplb@gmail.com>
2026-03-25 22:25:58 +0200

    allow any settings without requiring to have something specific.

--- src/models/base.ts
+++ src/models/base.ts
@@ -2,10 +2,4 @@
  * Represents the base settings structure for a mod.
  * Present for all mods.
  */
-export type BaseSettingsModel = {
-  /** Whether the mod is currently active. */
-  modEnabled: boolean;
-  
-  /** Whether to display a notification when a new version is detected. */
-  doShowNewVersionMessage: boolean;
-};
+export type BaseSettingsModel = Record<string, unknown>;
